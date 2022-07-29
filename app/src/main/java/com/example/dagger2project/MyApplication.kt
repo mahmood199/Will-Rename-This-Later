df0b3f9c4194df5ec746e0412b232ad77b19aa6e
MSI KATANA <mohammed.mahmood@semaai.com>
2022-07-30 00:58:51 +0530

    Adding Application class to the app

---
+++ app/src/main/java/com/example/dagger2project/MyApplication.kt
@@ -0,0 +1,14 @@
+package com.example.dagger2project
+
+import android.app.Application
+
+class MyApplication : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+
+
+}
