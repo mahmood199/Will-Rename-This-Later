ae8a9e7e234ff3282fbabb1e844dcabe42e1619d
MSI KATANA <mohammed.mahmood@semaai.com>
2022-07-30 16:46:09 +0530

    ##Injectio setup 1) Application name added to Manifest 2) Injecting a model globally 3) Added data binding in project 4) Added Module to component for injection 5) DI working fine!!!

--- app/src/main/java/com/example/dagger2project/ApplicationComponent.kt
+++ app/src/main/java/com/example/dagger2project/ApplicationComponent.kt
@@ -3,7 +3,8 @@ package com.example.dagger2project
 import dagger.Component
 import javax.inject.Singleton
 
-@Component
+@Singleton
+@Component(modules = [ApplicationModule::class])
 interface ApplicationComponent {
 
     fun inject(myApplication: MyApplication)
